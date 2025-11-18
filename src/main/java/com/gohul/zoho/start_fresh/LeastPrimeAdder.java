468e5cf08b9968108550446d50a0a156b4668a17
Gohul K <kpgohul@gmail.com>
2025-11-18 12:37:43 +0530

    added new roblm

--- src/main/java/com/gohul/zoho/start_fresh/LeastPrimeAdder.java
+++ src/main/java/com/gohul/zoho/start_fresh/LeastPrimeAdder.java
@@ -2,16 +2,17 @@ package com.gohul.zoho.start_fresh;
 
 public class LeastPrimeAdder {
 
-    public static void main(String[] args) {
-
-        int[] A = {10};
-        int[] B = {6, 9, 12};
-
-        System.out.println();
-    }
-
-    public static int findOut(int n, int[] list){
-
-        int R = n % list[0]
-    }
+//    public static void main(String[] args) {
+//
+//        int[] A = {10};
+//        int[] B = {6, 9, 12};
+//
+//        System.out.println();
+//    }
+//
+//
+//    public static int findOut(int n, int[] list){
+//
+//        int R = n % list[0]
+//    }
 }
